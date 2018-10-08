2a419200dca66af69e3479a6ff309187714a1ac8
patriciadeguzman <2015081674@ust-ics.mygbiz.com>
2018-10-08 14:15:41 +0800

    Final commit

--- app/src/main/java/com/deguzman/patricia/Main2.java
+++ app/src/main/java/com/deguzman/patricia/Main2.java
@@ -12,6 +12,7 @@ public class Main2 extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen2);
+        setTitle("Activity 2");
     }
 
     public void process(View v){
